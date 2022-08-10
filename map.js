6c3fb45d33c34ded1d364ef133f06cd61f62e37c
mohon63 <mohonhossen63@gmail.com>
2022-08-10 14:06:28 +0600

    mohon

--- map.js
+++ map.js
@@ -1,1 +1,25 @@
-helo js
+const numbers = [3, 5, 7, 9, 11];
+
+const output2 = [];
+
+const doubleIt = number => number * 2;
+for (const number of numbers) {
+    const result = doubleIt(number);
+    output2.push(result)
+}
+// console.log(output);
+
+// const output = numbers.map(doubleIt);
+const output = numbers.map(number => number * 3);
+console.log(output);
+
+const products = [
+    { name: 'water bottole', price: 50, colour: 'yellow' },
+    { name: 'sun glass', price: 150, colour: 'black' },
+    { name: 'watch', price: 100, colour: 'red' },
+    { name: 'mobile', price: 10000, colour: 'pink' },
+    { name: 'tShart', price: 550, colour: 'blue' }
+];
+
+const productName = products.map(product => product.name);
+console.log(productName)
